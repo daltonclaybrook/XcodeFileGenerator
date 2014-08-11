75c5f7351b04364214f145cd545b1c870ac19161
Dalton Claybrook <dalton.claybrook@bottlerocketapps.com>
2014-08-10 21:17:48 -0500

    Initial commit

---
+++ XcodeFileGenerator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  XcodeFileGenerator
+//
+//  Created by Dalton Claybrook on 8/10/14.
+//  Copyright (c) 2014 Space Factory Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
